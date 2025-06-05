2ed85604a7036cc314bb572dc34e644bc7eed0d3
Jeff Ma <jeff.ma713@gmail.com>
2025-06-04 20:34:48 -0700

    Fix renderer.cpp where initialization failed

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main() {
     }
 
     resources::ResourceManager resourceManager(renderer.GetSDLRenderer());
-    SDL_Texture* texture = resourceManager.LoadTexture("../assets/test.jpg"); // 确保该图存在
+    SDL_Texture* texture = resourceManager.LoadTexture("../assets/test.jpg");
 
     if (!texture) {
         return 1;
